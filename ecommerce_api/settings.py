e76271af58e3b14c915cd70f79f29d3fb4c2e801
myk4040okothogodo <mikeogodo5@gmail.com>
2022-01-28 18:28:34 +0300

    dockernetworking

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     }
 }
 
-
+ALLOWED_HOSTS = ['127.0.0.1','0.0.0.0','localhost']
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 

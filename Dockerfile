c37e4c46dc420d09d7ebca2a83fa2268751995e9
myk4040okothogodo <mikeogodo5@gmail.com>
2022-01-28 16:33:24 +0300

    usermodel

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.6
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+RUN  mkdir /ecommerce_api
+WORKDIR  /ecommerce_api
+
+COPY . /ecommerce_api/
+RUN pip install -r requirements.txt
---
+++ user_controller/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Email is required to create this user")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.sefdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if not extra_fields.get('is_staff', False):
+            raise ValueError('Superuser must have is_staff=True.')
+
+        if not extra_fields.get('is_superuser', False):
+            raise ValueError('Superuser must have is_staff=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default= True)
+
+    USERNAME_FIELD = "email"
+    objects = UserManager()
+
+
+    def __str__(self):
+        return self.email
+
+
+    def save(self, *args, **kwargs):
+        super().full_clean()
+        super().save(*args, **kwargs)
+
